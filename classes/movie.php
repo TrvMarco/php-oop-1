8064692599b2d2417d9c3ae32a1e1485037c35a9
TrvMarco <trevisanello.marco@gmail.com>
2022-06-27 17:22:03 +0200

    aggiunto films

--- classes/movie.php
+++ classes/movie.php
@@ -5,6 +5,16 @@ class Movie{
     public $genre;
     public $producer;
     public $length;
-    public $relase_date; 
+    public $relase_year; 
     public $price;
+
+    public function __construct($_title, $_producer, $_genre, $_price,$_relase_year, $_length)
+    {
+        $this->title = $_title;
+        $this->producer = $_producer;
+        $this->genre = $_genre;
+        $this->price = $_price;
+        $this->relase_year = $_relase_year;
+        $this->length = $_length;
+    }
 }
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once __DIR__ . '/classes/movie.php';
+    
+    $movies = [
+        new Movie('Joker','Todd Phillips','Azione', 10, '2019', 121),
+        new Movie('Interstellar','Christopher Nolan','Fantascenza', 12,'2014', 169),
+        new Movie('Slender Man', 'Sylvain White', 'Horror',7, '2018', 93),
+        new Movie('Una notte da leoni', 'Todd Phillips','Commedia', 9, '2009', 99)
+    ]
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
